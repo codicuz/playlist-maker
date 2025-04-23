0b8f555d7cef55d6e7a5500e2c3da93d09ef3a61
Roman Balan <rabalan83@gmail.com>
2025-04-23 21:40:27 +0300

    Выполнено второе домашнее задание

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
+        val buttonMedia = findViewById<Button>(R.id.buttonMedia)
+        val buttonSettings = findViewById<Button>(R.id.buttonSettings)
+
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажато Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажато Медиатека!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажато Настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
