a46c2a1bcf13a323200dabe236058a926070da39
Roman Balan <rabalan83@gmail.com>
2025-05-05 00:31:08 +0300

    Добавлена верстка макета для активити search

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,22 +1,62 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.app.ActivityOptions
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
-    fun AppCompatActivity.restartWithTheme() {
+    private fun AppCompatActivity.restartWithTheme() {
         val intent = intent
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-
         val options = ActivityOptions.makeCustomAnimation(this, 0, 0)
         startActivity(intent, options.toBundle())
-
         finish()
+    }
+
+    private fun canHandleIntent(intent: Intent): Boolean {
+        val activities = packageManager.queryIntentActivities(intent, 0)
+        return activities.isNotEmpty()
+    }
+
+    private fun intentHandleError() {
+        Toast.makeText(this, getString(R.string.no_intent_handle), Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun callPracticumOfferIntent() {
+        val practicumOffer = getString(R.string.practicum_license)
+        val agreement = Intent(Intent.ACTION_VIEW)
+        agreement.data = Uri.parse(practicumOffer)
+
+        if (canHandleIntent(agreement)) startActivity(agreement) else intentHandleError()
+    }
+
+    private fun callSendToHelpdesk() {
+        val sendToHeldesk = Intent(Intent.ACTION_SENDTO)
+        sendToHeldesk.data = Uri.parse(getString(R.string.send_to))
+        sendToHeldesk.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email)))
+        sendToHeldesk.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+        sendToHeldesk.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text))
+
+//        if (canHandleIntent(sendToHeldesk)) startActivity(sendToHeldesk) else intentHandleError()
+        startActivity(sendToHeldesk)
+    }
+
+    private fun callShareApp() {
+        val shareApp = Intent(Intent.ACTION_SEND)
+        shareApp.type = getString(R.string.text_plain)
+        shareApp.putExtra(Intent.EXTRA_TEXT,
+            getString(R.string.https_practicum_yandex_ru_android_developer))
+
+        if (canHandleIntent(shareApp)) startActivity(shareApp) else intentHandleError()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,18 +65,11 @@ class SettingsActivity : AppCompatActivity() {
 
         val buttonBack = findViewById<TextView>(R.id.settingsHeader)
         val switch = findViewById<SwitchCompat>(R.id.night_theme_switch)
-        val prefs = getSharedPreferences("settings", MODE_PRIVATE)
+        val practicumOfferBtn = findViewById<TextView>(R.id.practicumOffer)
+        val sendToHelpdesk = findViewById<TextView>(R.id.sendToHelpdesk)
+        val shareApp = findViewById<TextView>(R.id.shareApp)
 
-//        val nightMode = AppCompatDelegate.getDefaultNightMode()
-//        switch.isChecked = nightMode == AppCompatDelegate.MODE_NIGHT_YES
-//
-//
-//        switch.setOnCheckedChangeListener { _, isChecked ->
-//            AppCompatDelegate.setDefaultNightMode(
-//                if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
-//            )
-//            restartWithTheme() // Перезапускаем Activity, чтобы применить тему
-//        }
+        val prefs = getSharedPreferences("settings", MODE_PRIVATE)
 
         val isDark = prefs.getBoolean("dark_mode", false)
         switch.isChecked = isDark
@@ -54,6 +87,18 @@ class SettingsActivity : AppCompatActivity() {
 
         buttonBack.setOnClickListener {
             finish()
+        }
+
+        practicumOfferBtn.setOnClickListener {
+            callPracticumOfferIntent()
+        }
+
+        sendToHelpdesk.setOnClickListener {
+            callSendToHelpdesk()
+        }
+
+        shareApp.setOnClickListener{
+            callShareApp()
         }
     }
 }
