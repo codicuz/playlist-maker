aa7d79db658efbfaf8c0049eb9148c805ce43ff9
Roman Balan <rabalan83@gmail.com>
2025-06-29 17:39:49 +0300

    Добавлен макет и активити аудиоплеера

---
+++ app/src/main/java/com/practicum/playlistmaker/AudioPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.practicum.playlistmaker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AudioPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.audio_player)
+    }
+
+}
