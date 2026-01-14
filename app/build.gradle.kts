45df7d6dfb815e913bc1c9d6b607aa3efdcfab58
Roman Balan <rabalan83@gmail.com>
2026-01-14 22:31:49 +0300

    tarrgetSdk -> 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.practicum.playlistmaker"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
