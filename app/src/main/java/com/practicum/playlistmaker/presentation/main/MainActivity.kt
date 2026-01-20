4873834ed8651211836c2e45764728cd785c4226
Roman Balan <rabalan83@gmail.com>
2026-01-21 02:58:20 +0300

    Перевод на фрагменты и навигацию

--- app/src/main/java/com/practicum/playlistmaker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/main/MainActivity.kt
@@ -3,6 +3,10 @@ package com.practicum.playlistmaker.presentation.main
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityMainBinding
 import com.practicum.playlistmaker.presentation.media.MediaActivity
 import com.practicum.playlistmaker.presentation.search.SearchActivity
@@ -17,26 +21,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonSearch.setOnClickListener {
-            startActivity(
-                Intent(
-                    this, SearchActivity::class.java
-                )
-            )
-        }
-        binding.buttonMedia.setOnClickListener {
-            startActivity(
-                Intent(
-                    this, MediaActivity::class.java
-                )
-            )
-        }
-        binding.buttonSettings.setOnClickListener {
-            startActivity(
-                Intent(
-                    this, SettingsActivity::class.java
-                )
-            )
-        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
+
+//        binding.buttonSearch.setOnClickListener {
+//            startActivity(
+//                Intent(
+//                    this, SearchActivity::class.java
+//                )
+//            )
+//        }
+//        binding.buttonMedia.setOnClickListener {
+//            startActivity(
+//                Intent(
+//                    this, MediaActivity::class.java
+//                )
+//            )
+//        }
+//        binding.buttonSettings.setOnClickListener {
+//            startActivity(
+//                Intent(
+//                    this, SettingsActivity::class.java
+//                )
+//            )
+//        }
     }
 }
