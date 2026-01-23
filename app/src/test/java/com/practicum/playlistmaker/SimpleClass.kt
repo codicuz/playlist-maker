6cf56678b585cd0f56c03b4adf76687a485de6fe
Roman Balan <rabalan83@gmail.com>
2026-01-24 02:41:21 +0300

    Добавлен простой тест

---
+++ app/src/test/java/com/practicum/playlistmaker/SimpleClass.kt
@@ -0,0 +1,11 @@
+package com.practicum.playlistmaker
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class SimpleClass {
+    @Test
+    fun someTest() {
+        assertEquals("text", "text")
+    }
+}
