06003fdeb3534fdf37e85f74dc2006201fbca14b
Roman Balan <rabalan83@gmail.com>
2026-01-28 02:57:59 +0300

    Дом зад

--- app/src/main/java/com/practicum/playlistmaker/presentation/media/PlaylistsFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/presentation/media/PlaylistsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
 
 class PlaylistsFragment : Fragment() {
@@ -17,6 +19,14 @@ class PlaylistsFragment : Fragment() {
     ): View {
         _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createPlaylistButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mediaFragment_to_newPlaylistFragment)
+        }
     }
 
     override fun onDestroyView() {
